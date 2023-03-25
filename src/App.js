d19e76681c7ed55f326a1d1142e4daa85b51b81b
Betul <btlkurumsal@gmail.com>
2023-03-26 01:16:21 +0300

    Soup_Restaurant_Master

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import data from './data.js'
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+
+      {data.map((item)=> <Main {...item}/>)}
+   
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
